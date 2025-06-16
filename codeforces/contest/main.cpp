04796f30a857bc17887e5630f8d736f3d799fa8c
Code8Soumya <ssenapati721@gmail.com>
2025-06-16 21:00:49 +0530

    restructuring

--- codeforces/contest/main.cpp
+++ codeforces/contest/main.cpp
@@ -1,13 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 
     
-
+    
     return 0;
 }
